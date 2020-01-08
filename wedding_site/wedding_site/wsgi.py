4c371cb129f73381ba9a9b2ba39ea4c01d5a2a89
Desiree <dchon@ivytech.edu>
2020-01-08 11:58:05 -0500

    modified

--- wedding_site/wedding_site/wsgi.py
+++ wedding_site/wedding_site/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wedding_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'wedding_site.wedding_site.settings')
 
 application = get_wsgi_application()
